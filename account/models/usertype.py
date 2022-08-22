4c9b344217ee869f04d1e5ac5d16dadf880f1b2a
Fateme <ansari.3392@gmail.com>
2022-08-22 23:39:30 +0430

    update usertype model

--- account/models/usertype.py
+++ account/models/usertype.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.db.models import Q
 
 
 class UserType(models.Model):
     name = models.CharField(max_length=150, unique=True)
     percent = models.FloatField(default=0)
+    is_default = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                condition=Q(is_default=True),
+                fields=['is_default'],
+                name="unique_default_user_type"
+            )
+        ]
