588d89e0032c0b16d06bda07fd1f3cd86817281c
Fateme <ansari.3392@gmail.com>
2022-08-22 13:23:12 +0430

    add change password view and serializer

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from account.api.views.change_password import ChangePasswordView
 from account.api.views.register import RegisterView
 
 app_name = 'api'
@@ -9,5 +10,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('access/', TokenObtainPairView.as_view(), name='access'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
 
 ]
