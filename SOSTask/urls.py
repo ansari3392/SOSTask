e2464a59bfe287f143cbccb5da73fa8f9e6a5c85
Fateme <ansari.3392@gmail.com>
2022-08-22 11:03:21 +0430

    install rest framework, add JWT settings and update requirements.txt

--- SOSTask/urls.py
+++ SOSTask/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('account.urls')),
+    path('access/', TokenObtainPairView.as_view(), name='access'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
