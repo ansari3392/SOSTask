958015923a34b878cb95400fa11e3a69e2dc6d75
Fateme <ansari.3392@gmail.com>
2022-08-22 10:49:50 +0430

    add database settings and add it to requirements.txt

--- SOSTask/settings.py
+++ SOSTask/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SOSTask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'SOS',
+        'USER': 'postgres',
+        'PASSWORD': 123456,
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
